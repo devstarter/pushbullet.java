72d1d6866026c2c39cfd828cfad9e368488d66f6
Sllouyssgort <sllouyssgort@gmail.com>
2014-11-14 22:22:47 +0200

    improve test

--- src/test/main/java/com/pushbullet/PushbulletTest.java
+++ src/test/main/java/com/pushbullet/PushbulletTest.java
@@ -1,12 +1,11 @@
 package com.pushbullet;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
-import org.codehaus.jackson.map.ObjectMapper;
 
 import java.util.ArrayList;
 
@@ -15,28 +14,30 @@ import static com.pushbullet.Builder.registerClient;
 
 public class PushbulletTest {
 
-    private PushbulletClient pushbulletClient;
-
     @Before
     public void setup() {
-        pushbulletClient = new PushbulletClient();
+        PushbulletClient pushbulletClient = new PushbulletClient();
         pushbulletClient.key = System.getenv("PUSHBULLET_API");
 
-        final ArrayList<HttpMessageConverter<?>> converters = new ArrayList<>();
+        final ArrayList<HttpMessageConverter<?>> converters = new ArrayList<HttpMessageConverter<?>>();
         MappingJacksonHttpMessageConverter jacksonConverter = new MappingJacksonHttpMessageConverter();
         jacksonConverter.setObjectMapper(new ObjectMapper());
         converters.add(jacksonConverter);
-        converters.add(new StringHttpMessageConverter());
 
         final RestTemplate template = new RestTemplate();
         template.setMessageConverters(converters);
         pushbulletClient.restTemplate = template;
+        registerClient(pushbulletClient);
     }
 
     @Test
-    public void note() throws Exception {
-        registerClient(pushbulletClient);
+    public void note() {
         pushbullet().pushes().channel("pushbullet-java").note("to chanel", "test");
         pushbullet().pushes().broadcast().note("broadcast", "test");
+    }
+
+    @Test
+    public void utf() {
+        pushbullet().pushes().broadcast().note("Привет", "Текст на русском");
     }
 }
